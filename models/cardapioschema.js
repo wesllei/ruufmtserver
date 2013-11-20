473b95c77915d8babc70732218e6e8fc2f67d14f
Wesllei Henrique <wesllei.h@gmail.com>
2013-11-20 03:34:45 -0300

    Scrap completo e salvando no banco

--- models/cardapioschema.js
+++ models/cardapioschema.js
@@ -2,11 +2,12 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var cardapioSchema = new Schema({
-	date : Date,
-	scrape : String
+	date : String,
+	scrape : Object
 });
-cardapioSchema.methods.exist = function(cb){
-	return this.model('Cardapio').find({ scrape: this.scrape }, cb); 
+cardapioSchema.statics.findByScrape = function(data, cb) {
+	this.find({
+		'date' : new RegExp(data, 'i')
+	}, cb);
 }
-
-module.exports = cardapioSchema; 
+module.exports = cardapioSchema;
--- routes/user.js
+++ routes/user.js
@@ -3,10 +3,6 @@
  * GET users listing.
  */
 
-exports.index = function(req, res){
+exports.list = function(req, res){
   res.send("respond with a resource");
-};
-
-exports.add = function(req, res){
-  res.send("Data to add");
 };
